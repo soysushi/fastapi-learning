e657817ba72fcf64b9e02d5864b72e98c1bfbcca
soysushi <chi_oscar@hotmail.com>
2022-03-03 18:46:45 -0800

    CRUD-API

--- main.py
+++ main.py
@@ -64,5 +64,22 @@ def delete_post(id: int):
     # my_posts.pop(index)
     index = find_index_post(id)
 
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id: {id} does not exists")
+
     my_posts.pop(index)
-    return {'message': 'post was successfully deleted'}
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id: {id} does not exists")
+    
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"data": post_dict}
